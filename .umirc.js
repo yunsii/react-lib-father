9ea68c0e1dd431f83c84b17f75b928c99e0f4303
zpr1g <yuns.xie@qq.com>
2020-06-11 22:38:06 +0800

    feat: github pages deploy action

--- .umirc.js
+++ .umirc.js
@@ -11,5 +11,7 @@ export default {
       },
     ],
   ],
-  hash: true,
+  base: '/react-lib-father',
+  publicPath: '/react-lib-father/',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
 };
